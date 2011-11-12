d7709671fbdbf511d5ce1ec5a2fd269f759c4e24
Monica Wilkinson <monica.keller@gmail.com>
2011-11-11 16:00:21 -0800

    Changes to use vmc client directly

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'http://rubygems.org'
 
 gem 'sinatra'
+gem 'rack-flash'
 gem 'json'
 gem 'haml'
 gem 'vmc'
+gem 'zip', "~> 2.0.2"
 
--- sample.rb
+++ sample.rb
@@ -1,22 +1,67 @@
 require 'rubygems'
 require 'sinatra'
 require 'json'
-require 'cfmeta'
+require 'vmc/client'
+require 'rack-flash'
 
 enable :sessions
+use Rack::Flash
 
 configure do
-  
+  @@target = "api.cloudfoundry.com"
 end
 
 before do
+
+  # Example of how to only allow https
+  unless (ENV['bypass_ssl'] && request.secure?)
+    halt 401, 'This website can only be accessed over SSL'
+  end
+
   @title = "Developers"
   @canonical_url = request.url
   @fb_app_id = ENV['facebook_app_id']
+
+  if (session[:auth_token] && !session[:email])
+    @vmcclient = VMC::Client.new(@@target, session[:auth_token])
+    begin
+      if (info = @vmcclient.info)
+        session[:email] = info['user']
+      end
+    rescue
+      session.delete :auth_token
+    end
+  end
+end
+
+post '/login' do
+  email = params[:email]
+  password = params[:password]
+
+  if (email && password)
+    @vmcclient = VMC::Client.new(@@target)
+    begin
+      @vmcclient.login(email, password)
+      session[:auth_token] = @vmcclient.auth_token
+      session[:email] = email
+    rescue Exception => ex
+      flash[:error] =  "Login Failed"
+    end
+  else
+    flash[:error] = "Fill out the form"
+  end
+  redirect '/'
+end
+
+get '/logout' do
+  session.delete :auth_token
+  session.delete :email
+  redirect '/'
 end
 
 get '/' do
   haml :index
+
 end
 
 
