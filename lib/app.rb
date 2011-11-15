89c4a3780fbc40a9b30bd4f72cb3c0ccdfc9a136
Monica Wilkinson <monica.keller@gmail.com>
2011-11-14 18:29:42 -0800

    App Pushing Template

---
+++ lib/app.rb
@@ -0,0 +1,27 @@
+require 'mongoid'
+
+class App
+  include Mongoid::Document
+  #include Mongoid::Timestamps
+  #
+  #has_and_belongs_to_many :service_infos
+  #has_many :app_health_snapshots
+  #belongs_to :ogp_description
+
+  field :app_id, :type => Integer
+  field :app_urls, :type => Array
+  field :display_name, :type => String
+  field :description, :type => String
+  field :instances, :type => Integer, :default => 1
+  field :memory, :type => Integer, :default => 128
+  field :runtime, :type => String
+  field :framework, :type => String
+  field :git_repo, :type => String
+  field :browseable, :type => Boolean
+  field :cloneable, :type => Boolean
+
+  field :env_vars, :type => Hash
+
+  index :app_id, :unique => true
+  index :display_name, :unique => true
+end
--- sample.rb
+++ sample.rb
@@ -4,11 +4,18 @@ require 'json'
 require 'vmc/client'
 require 'rack-flash'
 
+
+require_relative 'lib/app'
+require_relative 'lib/cf_mongoid'
+
+include CloudFoundry
+
 enable :sessions
 use Rack::Flash
 
 configure do
   @@target = "api.cloudfoundry.com"
+  CloudFoundry::Mongo.config
 end
 
 before do
@@ -18,7 +25,7 @@ before do
   #  halt 401, 'This website can only be accessed over SSL'
   #end
 
-  @title = "Developers"
+  @title = "Gallery"
   @canonical_url = request.url
   @fb_app_id = ENV['facebook_app_id']
 
@@ -37,8 +44,6 @@ end
 post '/login' do
   email = params[:email]
   password = params[:password]
-
-  puts "Unsecure request BOOO" unless request.secure?
 
   if (email && password)
     @vmcclient = VMC::Client.new(@@target)
@@ -62,8 +67,58 @@ get '/logout' do
 end
 
 get '/' do
+  #TODO: Get from mongo db
+  @links = {}
+  @links['Ruby'] = {:display_name => "Ruby Box.net", :url => "/apps/boxnet-ruby"}
+  @links['Spring'] = {:display_name => "Spring Box.net", :url => "/apps/boxnet-spring"}
+
   haml :index
 
 end
+
+post '/apps/:app_name/new_copy' do |app_name|
+  @app_name = app_name
+  build_app if app_name ==  "boxnet-ruby"
+end
+
+get '/apps/:app_name/new_copy' do |app_name|
+  @app_name = app_name
+  build_app if app_name ==  "boxnet-ruby"
+end
+
+def build_app
+  # Find the app if its browsable
+  @app = App.new
+  @app.display_name = "Box.net Ruby Sample"
+  @app.app_urls = ["rub-sam.cloudfoundry.com"]
+  @title = @app.display_name
+  @app.description = "A starter Box.net application which showcases accessing files and folders as well as sharing."
+  @thumb_url = "http://veederline.com.au/wp-content/uploads/2011/07/digital_pub_box_net.jpg"
+  # Avatar, name, description
+
+  # GitHub Location
+  @app.git_repo =  "https://github.com/ciberch/sinatra-cloudfoundry-basic-website"
+  @zip_url = "#{@app.git_repo}/zipball/master"
+
+  parts =  @app.app_urls.first.split('.')
+  @new_name = parts[0] + rand(9999).to_i.to_s
+  if session[:email]
+    @new_name = parts[0] + session[:email].gsub('@', '.')
+  end
+  #services
+
+  #env vars
+  @env_vars = {}
+  @env_vars['Box API Key'] = "wewewewkejnweh34823u4b3r3jhb45k43j5lk45mt34knmt5k34ltml3453l4"
+
+  haml :new_copy
+end
+
+post '/apps/boxnet-ruby/do_copy' do
+
+end
+
+
+
 
 
