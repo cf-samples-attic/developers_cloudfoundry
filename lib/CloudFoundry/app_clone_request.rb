7810536ba5e710b7d0739fd9c4f589907cdec498
Monica Wilkinson <monica.keller@gmail.com>
2012-01-18 22:00:54 -0800

    Save the cloud and rename the username

--- lib/CloudFoundry/app_clone_request.rb
+++ lib/CloudFoundry/app_clone_request.rb
@@ -7,7 +7,8 @@ class AppCloneRequest
   field :request_email, :type => String
   field :request_app_name, :type => String
   field :cf_app_name, :type => String
-  field :username, :type => String
+  field :cf_username, :type => String
+  field :cloud, :type => String
   field :external_app_url, :type => String
 
   validates_presence_of :request_email
